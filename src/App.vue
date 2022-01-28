20b16180a9c1f7f9afe179624c8e4c68197694e0
LucSil <luciansilva351@gmail.com>
2022-01-28 08:58:11 +0100

    added all necessary files

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cont">
+    <h1>To-dos</h1>
+    <Todo />
+  </div>
+</template>
+
+<script>
+import Todo from "./components/Todo.vue";
+
+export default {
+  name: "App",
+  components: { Todo },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+#app h1 {
+  color: #c1deae;
+}
+.cont {
+  background: #219f94;
+  max-width: 400px;
+  min-height: 400px;
+  margin: 10px auto;
+  padding: 10px 0;
+  border-radius: 20px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,9 @@
+<template>
+  <input type="text" name="todo" id="todo" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
